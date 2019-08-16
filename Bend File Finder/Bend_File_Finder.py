c8ac06dfa35970da8c0b712df99d20d601870afb
Taylor Sahli <taylorsahli837@gmail.com>
2019-08-16 13:56:24 -0600

    Added error catching for user input

--- Bend File Finder/Bend_File_Finder.py
+++ Bend File Finder/Bend_File_Finder.py
@@ -3,21 +3,29 @@ from io import open
 import shutil
 import os
 
-while True: 
+print("This program will use a list of bends in .txt format and search a folder of bend reports. It will copy files that match the names on the list to a new folder.\nThis program, the list in .txt format, and a folder of .txt files must be in the same folder to run.\n")
+while True:
     try:
-        goodListName = raw_input("Enter filename of text files with wanted values: ")
+        goodListName = raw_input("Enter filename of text file with wanted values: ")
         goodListName = goodListName + ".txt"
         goodListFile = open(goodListName, "rb").read()
         break
     except:
-        print("File couldn't be opened!\nYour input is case sensitive.\nThis script needs to be in the same folder as the list of wanted values.\n")
+        print("File couldn't be opened!\nCheck your spelling.\nThis script needs to be in the same folder as the list of wanted values.\n")
 
 mytext = goodListFile.decode('utf16')
 wantedValues = mytext.splitlines()
 textExtension = ".txt"
 cwd = os.getcwd()
-newDirInput = raw_input("Enter name of new folder to create: ")
-os.mkdir(cwd + "/" + newDirInput)
+
+while True:
+    try:
+        newDirInput = raw_input("Enter name of new folder to create: ")
+        os.mkdir(cwd + "/" + newDirInput)
+        break
+    except:
+        print("\nThis folder already exists! Use another name.\n")
+
 newDir = cwd + "/" + newDirInput
 newVals = []
 
@@ -30,11 +38,11 @@ while True:
     try:
         folderToSearchName = raw_input("Enter the name of the folder to search: ")
         searchPath = "./" + folderToSearchName
+        for file in os.listdir(searchPath):
+            if file in newVals:
+                shutil.copy2(searchPath + "/" + file, newDir)
+            else: continue
         break
     except:
         print("Directory not found!")
-
-for file in os.listdir(searchPath):
-    if file in newVals:
-        shutil.copy2(searchPath + "/" + file, newDir)
-    else: continue
+raw_input("-------DONE: PRESS ENTER TO EXIT-------")
